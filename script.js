12ab8513d76cd2836f2cbfa0fd8abd4c2feaf352
AtamTristan <tristan.wesendahl@gmail.com>
2023-06-13 22:08:52 +0200

    8-directional sprite animation

--- script.js
+++ script.js
@@ -23,8 +23,16 @@ window.addEventListener('load', function(){
 			this.dx = 0;
 			this.dy = 0;
 			this.speedModifier = 5;
+			this.spriteWidth = 255;
+			this.spriteHeight = 255;
+			this.width = this.spriteWidth;
+			this.height = this.spriteHeight;
+			this.spriteX = this.collisionX - this.width * 0.5;
+			this.spriteY = this.collisionY - this.height * 0.5;
+			this.image = document.getElementById('bull');
 		}
 		draw(context){
+			context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
 			context.beginPath(); // for drawing a new shape and close previous
 			context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2); // at least 5 arguments
 			/*
@@ -61,6 +69,8 @@ window.addEventListener('load', function(){
 			}
 			this.collisionX += this.speedX * this.speedModifier;
 			this.collisionY += this.speedY * this.speedModifier;
+			this.spriteX = this.collisionX - this.width * 0.5;
+			this.spriteY = this.collisionY - this.height * 0.5 - 100;
 			// collisions with obstacles
 			this.game.obstacles.forEach(obstacle => {
 				//[(distance < sunOfRadii), distance, sunOfRadii, dx, dy]
